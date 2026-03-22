b823158fc176dc9cf1e8c310c6fe721db4c22eff
adamdev <adamdevofficial@protonmail.com>
2026-03-22 19:57:13 -0400

    Changed attack patch to use the attack struct

--- src/Patches/AttackPatch.cs
+++ src/Patches/AttackPatch.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
-
+using HarmonyLib;
 using UnityEngine;
 
 using MelonLoader;
@@ -13,78 +13,13 @@ using Il2CppSLZ.Marrow.Data;
 
 namespace NEP.DOOMLAB.Patches
 {
+    [HarmonyPatch(typeof(ImpactProperties), nameof(ImpactProperties.ReceiveAttack))]
     internal static class AttackPatch
     {
         public static Action<Attack> OnAttackReceived;
-
-        private static AttackPatchDelegate _original;
-        private delegate void AttackPatchDelegate(IntPtr instance, IntPtr attack, IntPtr method);
-
-        public static unsafe void Patch()
+        public static unsafe void Postfix(ImpactProperties __instance, Attack attack)
         {
-            AttackPatchDelegate patch = Patch;
-
-            string nativeInfoName = "NativeMethodInfoPtr_ReceiveAttack_Public_Virtual_Final_New_Void_Attack_0";
-            var tgtPtr = *(IntPtr*)(IntPtr)typeof(ImpactProperties).GetField(nativeInfoName, BindingFlags.Static | BindingFlags.NonPublic).GetValue(null);
-            var dstPtr = patch.Method.MethodHandle.GetFunctionPointer();
-            MelonUtils.NativeHookAttach((IntPtr)(&tgtPtr), dstPtr);
-            _original = Marshal.GetDelegateForFunctionPointer<AttackPatchDelegate>(tgtPtr);
-        }
-
-        public static void Patch(IntPtr instance, IntPtr attack, IntPtr method)
-        {
-            _original(instance, attack, method);
-
-            unsafe
-            {
-                try
-                {
-                    Attack_ refAttack = *(Attack_*)attack;
-
-                    Attack _attack = new Attack()
-                    {
-                        damage = refAttack.damage,
-                        normal = refAttack.normal,
-                        origin = refAttack.origin,
-                        backFacing = refAttack.backFacing == 1 ? true : false,
-                        OrderInPool = refAttack.OrderInPool,
-                        collider = refAttack.Collider,
-                        attackType = refAttack.attackType,
-                        proxy = refAttack.Proxy
-                    };
-
-                    OnAttackReceived?.Invoke(_attack);
-                }
-                catch
-                {
-
-                }
-            }
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct Attack_
-        {
-            public float damage;
-            public Vector3 normal;
-            public Vector3 origin;
-            public byte backFacing;
-            public int OrderInPool;
-            public IntPtr collider;
-            public AttackType attackType;
-            public IntPtr proxy;
-
-            public Collider Collider
-            {
-                get => new Collider(collider);
-                set => collider = value.Pointer;
-            }
-
-            public TriggerRefProxy Proxy
-            {
-                get => new TriggerRefProxy(proxy);
-                set => proxy = value.Pointer;
-            }
+            OnAttackReceived?.Invoke(attack);
         }
     }
 }
